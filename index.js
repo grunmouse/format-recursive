5da84c4e4d71c8c1ebb9d8316f221202a31c77e0
Dmitry Rakov <rakov@mirapolis.ru>
2020-03-03 18:03:13 +0300

    normal

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 
-const translator = require('./syntaxer.js');
-
-const lexer = require('./lexer.js');
-
-const factory = require('./factory.js');
-
 const compil = require('./compil.js');
 
 const Frame = require('./env.js');
 
-const fs = require('fs');
-
-let tmp = fs.readFileSync('..\\db-book\\db-files\\vers-0\\template\\setlost.sql', {encoding:'UTF-8'});
-let fromNone = fs.readFileSync('..\\db-book\\db-files\\vers-0\\template\\from\\from-none.sql', {encoding:'UTF-8'});
+const escape = require('./escape.js');
 
 
-let fra = new Frame();
+function use(tmpl, vars){
+	const fra = new Frame();
+	if(vars) for(let a in vars){
+		fra.set(a, vars[a]);
+	}
+	const fun = compil(tmpl);
+	
+	return escape.toText(fun(fra));
+}
 
-fra.set('fromNone', fromNone);
-fra.set('storageid', '123456789');
+function compile(tmpl){
+	const fun = compil(tmpl);
+	return (vars)=>{
+		const fra = new Frame();
+		if(vars) for(let a in vars){
+			fra.set(a, vars[a]);
+		}
+		return escape.toText(fun(fra));
+	}
+}
 
-//let a = lexer(fromNone);
 
-//[...a].forEach(a=>console.log(a));
-
-//console.log(fra.get('fromNone')(fra));
-
-let t = compil(tmp);
-
-console.log(t(fra));
+module.exports = {
+	escape,
+	compile,
+	use
+};
